9b344be14aea13a350eb6a361e98d77a80355833
asakurayonik <asakurayonik@gmail.com>
2015-12-06 20:41:36 -0500

    compress hashmap

--- Project/ReplicaManager/src/servant/loadHash.java
+++ Project/ReplicaManager/src/servant/loadHash.java
@@ -11,19 +11,20 @@ import java.util.List;
 import java.util.Map.Entry;
 import model2.Loan;
 import model2.Customer;
+import rm.ReplicaManager2;
 
 /**
  *
  * @author ni_ye
  */
 public class loadHash {
-    
-    /*public String hashToString(dlmsImpl bank)
-    {
+
+    public String hashToString(ReplicaManager2 rm) {
         String result = null;
-        for (String key : bank.accounts.keySet()) {
-            if (bank.accounts.get(key) != null) {
-                List<Customer> listCustomer = bank.accounts.get(key);
+
+        for (String key : rm.BankServantMap.get("A").accounts.keySet()) {
+            if (rm.BankServantMap.get("A").accounts.get(key) != null) {
+                List<Customer> listCustomer = rm.BankServantMap.get("A").accounts.get(key);
                 for (int i = 0; i < listCustomer.size(); i++) {
                     result += listCustomer.get(i).getAccountNumber() + "," + listCustomer.get(i).getFirstName() + "," + listCustomer.get(i).getLastName() + ","
                             + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + "|";
@@ -31,21 +32,61 @@ public class loadHash {
             }
         }
         result += "@";
-        for (Integer key : bank.loans.keySet()) {
-            if (bank.loans.get(key) != null) {              
-                result += bank.loans.get(key).getLoanId() + "," + bank.loans.get(key).getAccountNumber() + "," + bank.loans.get(key).getLoanAmt() 
-                        + bank.loans.get(key).getDueDate() + "|";  
+        for (Integer key : rm.BankServantMap.get("A").loans.keySet()) {
+            if (rm.BankServantMap.get("A").loans.get(key) != null) {
+                result += rm.BankServantMap.get("A").loans.get(key).getLoanId() + "," + rm.BankServantMap.get("A").loans.get(key).getAccountNumber() + "," + rm.BankServantMap.get("A").loans.get(key).getLoanAmt()
+                        + rm.BankServantMap.get("A").loans.get(key).getDueDate() + "|";
             }
-        }   
+        }
+        
+        result += "!";
+        
+        for (String key : rm.BankServantMap.get("B").accounts.keySet()) {
+            if (rm.BankServantMap.get("B").accounts.get(key) != null) {
+                List<Customer> listCustomer = rm.BankServantMap.get("B").accounts.get(key);
+                for (int i = 0; i < listCustomer.size(); i++) {
+                    result += listCustomer.get(i).getAccountNumber() + "," + listCustomer.get(i).getFirstName() + "," + listCustomer.get(i).getLastName() + ","
+                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + "|";
+                }
+            }
+        }
+        result += "@";
+        for (Integer key : rm.BankServantMap.get("B").loans.keySet()) {
+            if (rm.BankServantMap.get("B").loans.get(key) != null) {
+                result += rm.BankServantMap.get("B").loans.get(key).getLoanId() + "," + rm.BankServantMap.get("B").loans.get(key).getAccountNumber() + "," + rm.BankServantMap.get("B").loans.get(key).getLoanAmt()
+                        + rm.BankServantMap.get("B").loans.get(key).getDueDate() + "|";
+            }
+        }
+        
+        result += "!";
+        
+        for (String key : rm.BankServantMap.get("C").accounts.keySet()) {
+            if (rm.BankServantMap.get("C").accounts.get(key) != null) {
+                List<Customer> listCustomer = rm.BankServantMap.get("C").accounts.get(key);
+                for (int i = 0; i < listCustomer.size(); i++) {
+                    result += listCustomer.get(i).getAccountNumber() + "," + listCustomer.get(i).getFirstName() + "," + listCustomer.get(i).getLastName() + ","
+                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + "|";
+                }
+            }
+        }
+        result += "@";
+        for (Integer key : rm.BankServantMap.get("C").loans.keySet()) {
+            if (rm.BankServantMap.get("C").loans.get(key) != null) {
+                result += rm.BankServantMap.get("C").loans.get(key).getLoanId() + "," + rm.BankServantMap.get("C").loans.get(key).getAccountNumber() + "," + rm.BankServantMap.get("C").loans.get(key).getLoanAmt()
+                        + rm.BankServantMap.get("C").loans.get(key).getDueDate() + "|";
+            }
+        }
+        
         //System.out.println(result);
-        return result;    
+        return result;
     }
     
    
     
     public void stringToHash(String s, dlmsImpl bank)
     {
-        String elements[] = s.split("@");
+        String banks[] = s.split("!");
+        String elements[] = banks[0].split("@");
         String customers[] = elements[0].split("|");
         String loans[] = elements[1].split("|");
             
@@ -53,12 +94,11 @@ public class loadHash {
         {
             String token[] = customers[i].split(",");
             Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());    
-            Character key = token[1].trim().charAt(0);
+            String key = token[2].trim().substring(0, 1);
             List<Customer> customerList = bank.accounts.get(key);
             customerList.add(customer);
             bank.accounts.put(key, customerList);
-        }
-        
+        }    
         for (int i = 0; i<loans.length; i++)
         {
             String token[] = loans[i].split(",");
@@ -66,6 +106,48 @@ public class loadHash {
             Integer key = Integer.parseInt(token[0].trim());
             bank.loans.put(key, loan);
         }
-    }*/
+
+        String elements2[] = banks[1].split("@");
+        String customers2[] = elements2[0].split("|");
+        String loans2[] = elements2[1].split("|");
+        
+        for (int i = 0; i<customers2.length; i++)
+        {
+            String token[] = customers2[i].split(",");
+            Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());    
+            String key = token[2].trim().substring(0, 1);
+            List<Customer> customerList = bank.accounts.get(key);
+            customerList.add(customer);
+            bank.accounts.put(key, customerList);
+        }    
+        for (int i = 0; i<loans2.length; i++)
+        {
+            String token[] = loans2[i].split(",");
+            Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
+            Integer key = Integer.parseInt(token[0].trim());
+            bank.loans.put(key, loan);
+        }
+        
+        String elements3[] = banks[1].split("@");
+        String customers3[] = elements3[0].split("|");
+        String loans3[] = elements3[1].split("|");
+        
+        for (int i = 0; i<customers3.length; i++)
+        {
+            String token[] = customers3[i].split(",");
+            Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());    
+            String key = token[2].trim().substring(0, 1);
+            List<Customer> customerList = bank.accounts.get(key);
+            customerList.add(customer);
+            bank.accounts.put(key, customerList);
+        }    
+        for (int i = 0; i<loans3.length; i++)
+        {
+            String token[] = loans3[i].split(",");
+            Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
+            Integer key = Integer.parseInt(token[0].trim());
+            bank.loans.put(key, loan);
+        }
+    }
 }
 
--- Project/ReplicaManager/src/servant/loadHash2.java
+++ Project/ReplicaManager/src/servant/loadHash2.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import model.Account;
 import model.Loan;
+import rm.ReplicaManager1;
 
 /**
  *
@@ -17,10 +18,11 @@ import model.Loan;
 public class loadHash2 {
     
     
-     public String hashToString2(BankServant bank)
+     public String hashToString2(ReplicaManager1 rm)
     {
         String result = null;
-        for (ArrayList<Account> account_list : bank.account_HashMap.values())
+        
+        for (ArrayList<Account> account_list : rm.BankServantMap.get("A").account_HashMap.values())
         {
             for (Account account : account_list) {
                 result += account.accountNumber + "," + account.firstName + "," + account.lastName + "," + account.emailAddress + "," + account.phoneNumber
@@ -28,16 +30,46 @@ public class loadHash2 {
             }         
         }
         result += "@";
-        for (Loan temp : bank.loan_HashMap.values()) {
+        for (Loan temp : rm.BankServantMap.get("A").loan_HashMap.values()) {
             result += temp.ID + "," + temp.accountNumber + "," + temp.amount + "," + temp.dueDate + "|";
         }
-        //System.out.println(result);
+        
+        result += "!";
+        
+        for (ArrayList<Account> account_list : rm.BankServantMap.get("B").account_HashMap.values())
+        {
+            for (Account account : account_list) {
+                result += account.accountNumber + "," + account.firstName + "," + account.lastName + "," + account.emailAddress + "," + account.phoneNumber
+                        + "," + account.password + "|";
+            }         
+        }
+        result += "@";
+        for (Loan temp : rm.BankServantMap.get("B").loan_HashMap.values()) {
+            result += temp.ID + "," + temp.accountNumber + "," + temp.amount + "," + temp.dueDate + "|";
+        }
+        
+        result += "!";
+        
+        for (ArrayList<Account> account_list : rm.BankServantMap.get("C").account_HashMap.values())
+        {
+            for (Account account : account_list) {
+                result += account.accountNumber + "," + account.firstName + "," + account.lastName + "," + account.emailAddress + "," + account.phoneNumber
+                        + "," + account.password + "|";
+            }         
+        }
+        result += "@";
+        for (Loan temp : rm.BankServantMap.get("C").loan_HashMap.values()) {
+            result += temp.ID + "," + temp.accountNumber + "," + temp.amount + "," + temp.dueDate + "|";
+        }
+        
+        //System.out.println(result); 
         return result;
     }
      
     public void stringToHash2(String s, BankServant bank)
     {
-        String elements[] = s.split("@");
+        String banks[] = s.split("!");
+        String elements[] = banks[0].split("@");
         String customers[] = elements[0].split("|");
         String loans[] = elements[1].split("|");
             
@@ -49,11 +81,52 @@ public class loadHash2 {
             ArrayList<Account> customerList = bank.account_HashMap.get(key);
             customerList.add(customer);
             bank.account_HashMap.put(key, customerList);
-        }
-        
+        }     
         for (int i = 0; i<loans.length; i++)
         {
             String token[] = loans[i].split(",");
+            Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
+            String key = token[0].trim();
+            bank.loan_HashMap.put(key, loan);
+        }
+        
+        String elements2[] = banks[0].split("@");
+        String customers2[] = elements[0].split("|");
+        String loans2[] = elements[1].split("|");
+            
+        for (int i = 0; i<customers2.length; i++)
+        {
+            String token[] = customers2[i].split(",");
+            Account customer = new Account(token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim(),1000);    
+            String key = Character.toString(token[1].trim().charAt(0));
+            ArrayList<Account> customerList = bank.account_HashMap.get(key);
+            customerList.add(customer);
+            bank.account_HashMap.put(key, customerList);
+        }     
+        for (int i = 0; i<loans2.length; i++)
+        {
+            String token[] = loans2[i].split(",");
+            Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
+            String key = token[0].trim();
+            bank.loan_HashMap.put(key, loan);
+        }
+        
+        String elements3[] = banks[0].split("@");
+        String customers3[] = elements[0].split("|");
+        String loans3[] = elements[1].split("|");
+            
+        for (int i = 0; i<customers3.length; i++)
+        {
+            String token[] = customers3[i].split(",");
+            Account customer = new Account(token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim(),1000);    
+            String key = Character.toString(token[1].trim().charAt(0));
+            ArrayList<Account> customerList = bank.account_HashMap.get(key);
+            customerList.add(customer);
+            bank.account_HashMap.put(key, customerList);
+        }     
+        for (int i = 0; i<loans3.length; i++)
+        {
+            String token[] = loans3[i].split(",");
             Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
             String key = token[0].trim();
             bank.loan_HashMap.put(key, loan);
