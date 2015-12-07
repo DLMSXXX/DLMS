0a1a672be6d65ba43cc40851ee0605b2c75c3355
crownwangguan <crown.wg@outlook.com>
2015-12-06 23:42:43 -0500

    rm2

--- Project/ReplicaManager/src/rm/ReplicaManager2.java
+++ Project/ReplicaManager/src/rm/ReplicaManager2.java
@@ -90,82 +90,84 @@ public class ReplicaManager2 {
 
     }
 
-    public void stringToHash(String s, ReplicaManager2 replicaManager2)
-    {
+    public void stringToHash(String s, ReplicaManager2 replicaManager2) {
         String banks[] = s.split("!");
-        String elements[] = banks[0].split("@");
-        String customers[] = elements[0].split("|");
-        String loans[] = elements[1].split("|");
-            
-        for (int i = 0; i<customers.length; i++)
-        {
-            String token[] = customers[i].split(",");
-            Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());    
-            String key = token[2].trim().substring(0, 1);
-            List<Customer> customerList = replicaManager2.BankServantMap.get("A").accounts.get(key);
-            customerList.add(customer);
-            replicaManager2.BankServantMap.get("A").accounts.put(key, customerList);
-        }    
-        for (int i = 0; i<loans.length; i++)
-        {
-            String token[] = loans[i].split(",");
-            Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
-            Integer key = Integer.parseInt(token[0].trim());
-            replicaManager2.BankServantMap.get("A").loans.put(key, loan);
+        if (!banks[0].equals("@")) {
+            String elements[] = banks[0].split("@");
+            String customers[] = elements[0].split(";");
+            String loans[] = {};
+            if (elements.length > 1) {
+                loans = elements[1].split(";");
+            }
+            for (int i = 0; i < customers.length; i++) {
+                String token[] = customers[i].split(",");
+                Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());
+                String key = token[2].trim().substring(0, 1);
+                List<Customer> customerList = replicaManager2.BankServantMap.get("A").accounts.get(key);
+                customerList.add(customer);
+                replicaManager2.BankServantMap.get("A").accounts.put(key, customerList);
+            }
+            for (int i = 0; i < loans.length; i++) {
+                String token[] = loans[i].split(",");
+                Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
+                Integer key = Integer.parseInt(token[0].trim());
+                replicaManager2.BankServantMap.get("A").loans.put(key, loan);
+            }
         }
 
-        String elements2[] = banks[1].split("@");
-        String customers2[] = elements2[0].split("|");
-        String loans2[] = elements2[1].split("|");
-        
-        for (int i = 0; i<customers2.length; i++)
-        {
-            String token[] = customers2[i].split(",");
-            Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());    
-            String key = token[2].trim().substring(0, 1);
-            List<Customer> customerList = replicaManager2.BankServantMap.get("B").accounts.get(key);
-            customerList.add(customer);
-            replicaManager2.BankServantMap.get("B").accounts.put(key, customerList);
-        }    
-        for (int i = 0; i<loans2.length; i++)
-        {
-            String token[] = loans2[i].split(",");
-            Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
-            Integer key = Integer.parseInt(token[0].trim());
-            replicaManager2.BankServantMap.get("B").loans.put(key, loan);
+        if (!banks[1].equals("@")) {
+
+            String elements2[] = banks[1].split("@");
+            String customers2[] = elements2[0].split(";");
+            String loans2[] = elements2[1].split(";");
+
+            for (int i = 0; i < customers2.length; i++) {
+                String token[] = customers2[i].split(",");
+                Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());
+                String key = token[2].trim().substring(0, 1);
+                List<Customer> customerList = replicaManager2.BankServantMap.get("B").accounts.get(key);
+                customerList.add(customer);
+                replicaManager2.BankServantMap.get("B").accounts.put(key, customerList);
+            }
+            for (int i = 0; i < loans2.length; i++) {
+                String token[] = loans2[i].split(",");
+                Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
+                Integer key = Integer.parseInt(token[0].trim());
+                replicaManager2.BankServantMap.get("B").loans.put(key, loan);
+            }
         }
-        
-        String elements3[] = banks[1].split("@");
-        String customers3[] = elements3[0].split("|");
-        String loans3[] = elements3[1].split("|");
-        
-        for (int i = 0; i<customers3.length; i++)
-        {
-            String token[] = customers3[i].split(",");
-            Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());    
-            String key = token[2].trim().substring(0, 1);
-            List<Customer> customerList = replicaManager2.BankServantMap.get("C").accounts.get(key);
-            customerList.add(customer);
-            replicaManager2.BankServantMap.get("C").accounts.put(key, customerList);
-        }    
-        for (int i = 0; i<loans3.length; i++)
-        {
-            String token[] = loans3[i].split(",");
-            Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
-            Integer key = Integer.parseInt(token[0].trim());
-            replicaManager2.BankServantMap.get("C").loans.put(key, loan);
+
+        if (!banks[2].equals("@")) {
+            String elements3[] = banks[1].split("@");
+            String customers3[] = elements3[0].split(";");
+            String loans3[] = elements3[1].split(";");
+
+            for (int i = 0; i < customers3.length; i++) {
+                String token[] = customers3[i].split(",");
+                Customer customer = new Customer(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim(), token[4].trim(), token[5].trim());
+                String key = token[2].trim().substring(0, 1);
+                List<Customer> customerList = replicaManager2.BankServantMap.get("C").accounts.get(key);
+                customerList.add(customer);
+                replicaManager2.BankServantMap.get("C").accounts.put(key, customerList);
+            }
+            for (int i = 0; i < loans3.length; i++) {
+                String token[] = loans3[i].split(",");
+                Loan loan = new Loan(token[0].trim(), token[1].trim(), token[2].trim(), token[3].trim());
+                Integer key = Integer.parseInt(token[0].trim());
+                replicaManager2.BankServantMap.get("C").loans.put(key, loan);
+            }
         }
     }
-    
+
     public String hashToString(ReplicaManager2 rm) {
-        String result = null;
+        String result = "";
 
         for (String key : rm.BankServantMap.get("A").accounts.keySet()) {
             if (rm.BankServantMap.get("A").accounts.get(key) != null) {
                 List<Customer> listCustomer = rm.BankServantMap.get("A").accounts.get(key);
                 for (int i = 0; i < listCustomer.size(); i++) {
                     result += listCustomer.get(i).getAccountNumber() + "," + listCustomer.get(i).getFirstName() + "," + listCustomer.get(i).getLastName() + ","
-                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + "|";
+                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + ";";
                 }
             }
         }
@@ -173,18 +175,18 @@ public class ReplicaManager2 {
         for (Integer key : rm.BankServantMap.get("A").loans.keySet()) {
             if (rm.BankServantMap.get("A").loans.get(key) != null) {
                 result += rm.BankServantMap.get("A").loans.get(key).getLoanId() + "," + rm.BankServantMap.get("A").loans.get(key).getAccountNumber() + "," + rm.BankServantMap.get("A").loans.get(key).getLoanAmt()
-                        + rm.BankServantMap.get("A").loans.get(key).getDueDate() + "|";
+                        + rm.BankServantMap.get("A").loans.get(key).getDueDate() + ";";
             }
         }
-        
+
         result += "!";
-        
+
         for (String key : rm.BankServantMap.get("B").accounts.keySet()) {
             if (rm.BankServantMap.get("B").accounts.get(key) != null) {
                 List<Customer> listCustomer = rm.BankServantMap.get("B").accounts.get(key);
                 for (int i = 0; i < listCustomer.size(); i++) {
                     result += listCustomer.get(i).getAccountNumber() + "," + listCustomer.get(i).getFirstName() + "," + listCustomer.get(i).getLastName() + ","
-                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + "|";
+                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + ";";
                 }
             }
         }
@@ -192,18 +194,18 @@ public class ReplicaManager2 {
         for (Integer key : rm.BankServantMap.get("B").loans.keySet()) {
             if (rm.BankServantMap.get("B").loans.get(key) != null) {
                 result += rm.BankServantMap.get("B").loans.get(key).getLoanId() + "," + rm.BankServantMap.get("B").loans.get(key).getAccountNumber() + "," + rm.BankServantMap.get("B").loans.get(key).getLoanAmt()
-                        + rm.BankServantMap.get("B").loans.get(key).getDueDate() + "|";
+                        + rm.BankServantMap.get("B").loans.get(key).getDueDate() + ";";
             }
         }
-        
+
         result += "!";
-        
+
         for (String key : rm.BankServantMap.get("C").accounts.keySet()) {
             if (rm.BankServantMap.get("C").accounts.get(key) != null) {
                 List<Customer> listCustomer = rm.BankServantMap.get("C").accounts.get(key);
                 for (int i = 0; i < listCustomer.size(); i++) {
                     result += listCustomer.get(i).getAccountNumber() + "," + listCustomer.get(i).getFirstName() + "," + listCustomer.get(i).getLastName() + ","
-                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + "|";
+                            + listCustomer.get(i).getEmail() + "," + listCustomer.get(i).getPhoneNumber() + "," + listCustomer.get(i).getPassword() + ";";
                 }
             }
         }
@@ -211,14 +213,14 @@ public class ReplicaManager2 {
         for (Integer key : rm.BankServantMap.get("C").loans.keySet()) {
             if (rm.BankServantMap.get("C").loans.get(key) != null) {
                 result += rm.BankServantMap.get("C").loans.get(key).getLoanId() + "," + rm.BankServantMap.get("C").loans.get(key).getAccountNumber() + "," + rm.BankServantMap.get("C").loans.get(key).getLoanAmt()
-                        + rm.BankServantMap.get("C").loans.get(key).getDueDate() + "|";
+                        + rm.BankServantMap.get("C").loans.get(key).getDueDate() + ";";
             }
         }
-        
+
         //System.out.println(result);
         return result;
     }
-    
+
     private class RMReceiver implements Runnable {
 
         int RMport;
@@ -276,7 +278,7 @@ public class ReplicaManager2 {
                                 String ask = "ASK";
                                 DatagramPacket datagramPacket = new DatagramPacket(ask.getBytes(), _port);
                                 serverSocket.send(datagramPacket);
-                                
+
                                 serverSocket.receive(receivePacket);
                                 receivePacket = new DatagramPacket(receiveData, _port);
                                 String data = new String(receivePacket.getData());
